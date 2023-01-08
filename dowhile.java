7446b6fd6ec10318d36c7fc56d38293aad26f19d
ishabellelmisaa <118729310+ishabellelmisaa@users.noreply.github.com>
2023-01-08 23:12:01 +0700

    Create dowhile.java

---
+++ dowhile.java
@@ -0,0 +1,13 @@
+public class dowhile {
+    public static void main(String[]args)
+    {
+        String[] hari = { "Senin","Selasa","Rabu","Kamis","Jumat","Sabtu","Minggu"};
+
+        int i=0;
+        do
+            {
+                System.out.println(hari[i]);
+                i++;
+            }while(i<hari.length);
+    }
+}
