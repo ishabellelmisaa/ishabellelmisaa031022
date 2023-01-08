3aae101d375b7e3bf746d3d8eb48ed6f191fd4ee
ishabellelmisaa <118729310+ishabellelmisaa@users.noreply.github.com>
2023-01-08 23:11:03 +0700

    Create bukualamat.java

---
+++ bukualamat.java
@@ -0,0 +1,16 @@
+public class bukualamat {
+     public static void main(String[] args) 
+     {
+         String alamat[][] = {{"Florence", "735-1234", "Manila"}, {"Joyce", "983-3333", "Quezon City"},{"Becca", "456-3322", "Manila"}};
+         int i=0;
+
+         do{
+             System.out.println("name    :" + alamat[i][0] );
+             System.out.println("Tel. #  :" + alamat[i][1] );
+             System.out.println("Address :" + alamat[i][2] );
+             System.out.println(" ");
+             i++;
+        }
+         while (i<3);
+     }
+}
